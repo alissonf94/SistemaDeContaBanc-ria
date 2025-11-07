111d23124a77ffbfc6f95437a7863c5be3a8ad1a
Alissonf94 <alisson.fernandes94@outlook.com.br>
2025-11-07 20:01:12 -0300

    erro: falta de implementação da função de deposito

--- ContaBancariaTest.py
+++ ContaBancariaTest.py
@@ -4,3 +4,14 @@ from ContaBancaria import ContaBancaria
 def test_deve_ter_saldo_inicial_zero():
     conta = ContaBancaria()
     assert conta.get_saldo() == 0.0
+
+def test_deve_aumentar_saldo_apos_deposito_valido():
+
+    conta = ContaBancaria()
+    valor_deposito = 100.0
+
+    
+    conta.depositar(valor_deposito) 
+
+    saldo_esperado = 100.0
+    assert conta.get_saldo() == saldo_esperado
